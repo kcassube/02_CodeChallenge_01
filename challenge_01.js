6090e7440eae4de9683367b2b7a3500e1fd17858
Karsten <79251426+kcassube@users.noreply.github.com>
2023-07-13 13:01:56 +0200

    Update challenge_01.js

--- challenge_01.js
+++ challenge_01.js
@@ -9,3 +9,25 @@ Geben Sie die Summe der Zahlen in die Konsole aus:
 "Die Summe der Zahlen ist: summe"
 */
 
+
+/*********   CODE CHALLENGE 01 **********/
+ 
+/*
+Geben Sie über eine Prompt-Anforderung eine Zahl ein.
+Geben Sie über eine Prompt-Anforderung eine zweite Zahl ein.
+Berechnen Sie die Summe der beiden Zahlen.
+Geben Sie die Summe der Zahlen in die Konsole aus:
+"Die Summe der Zahlen ist: summe"
+*/
+
+// Zahl 1 einlesen
+var zahl1 = prompt("Geben Sie die erste Zahl ein:");
+
+// Zahl 2 einlesen
+var zahl2 = prompt("Geben Sie die zweite Zahl ein:");
+
+// Summe berechnen
+var summe = parseFloat(zahl1) + parseFloat(zahl2);
+
+// Summe ausgeben
+console.log("Die Summe der Zahlen ist: " + summe);
